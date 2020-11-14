12c955578087689b9b9545dcdc141aaa03abc0e1
ryok <lazmond3@ex.com>
2020-11-14 11:23:44 +0900

    add public icon and modify webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,33 @@
-import { ConfigurationFactory } from 'webpack'
+// import { ConfigurationFactory } from 'webpack'
 import path from 'path'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 
-const config: ConfigurationFactory = () => {
-  return {
+module.exports = {
     entry: {
-      content_scripts: path.join(__dirname, 'src', 'content_scripts.ts')
+        content_scripts: path.join(__dirname, 'src', 'content_scripts.ts')
     },
     output: {
-      // distディレクトリにcontent_scripts.jsを吐く
-      path: path.join(__dirname, 'dist'),
-      filename: '[name].js'
+        // distディレクトリにcontent_scripts.jsを吐く
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
     },
     module: {
-      rules: [
-        {
-          test: /.ts$/,
-          use: 'ts-loader',
-          exclude: '/node_modules/'
-        }
-      ]
+        rules: [
+            {
+                test: /.ts$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
     },
     resolve: {
-      extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js']
     },
     plugins: [
-      // publicディレクトリにあるファイルをdistディレクトリにコピーする
-      new CopyWebpackPlugin([
-        { from: 'public', to: '.' }
-      ])
+        // publicディレクトリにあるファイルをdistディレクトリにコピーする
+        new CopyWebpackPlugin({
+            patterns: [{ from: 'public', to: '.', }],
+        })
     ]
-  }
 }
 
-export default config
