c1e0c9dae3052aae7f2ae8ba5e4e34a095a29a21
ryok <lazmond3@ex.com>
2020-11-14 12:12:44 +0900

    get some keyboard events

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,8 @@ import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 module.exports = {
     entry: {
-        content_scripts: path.join(__dirname, 'src', 'content_scripts.ts')
+        content_scripts: path.join(__dirname, 'src', 'content_scripts.ts'),
+        background: path.join(__dirname, 'src', 'background.ts')
     },
     output: {
         // distディレクトリにcontent_scripts.jsを吐く
