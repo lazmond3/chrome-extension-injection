dfee5e133972b4367342e0292c6b16b5f3be13ea
ryok <lazmond3@ex.com>
2020-11-14 12:46:17 +0900

    fix copy text function but これはユーザアクションによってからしか起動しない

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -1,11 +1,12 @@
+import { commands } from './constValues';
 import { browser } from 'webextension-polyfill-ts'
 
 const execute = async () => {
-  const value = await browser.storage.local.get('date')
-  console.log(value.date || '日時が記録されていません')
+    const value = await browser.storage.local.get('date')
+    console.log(value.date || '日時が記録されていません')
 
-  await browser.storage.local.set({ date: new Date().toString() })
-  console.log('現在の日時を記録しました')
+    await browser.storage.local.set({ date: new Date().toString() })
+    console.log('現在の日時を記録しました')
 }
 
 execute()
@@ -14,7 +15,30 @@ execute()
 
 // これってbrowserでいいの？
 browser.runtime.onMessage.addListener(
-    (message:any, sender:any) => {
+    (message: string, sender: any) => {
         console.log(`message: ${message}`)
+        if (message === commands.COPY_TITLE) {
+            (async () => {
+                const value = await browser.storage.local.get('date');
+                console.log(value.date || '日時が記録されていません');
+            
+                const sampleTextPromise = await browser.storage.local.get("date")
+                const sampleText = sampleTextPromise.date as string
+                console.log(`sample text: ${sampleText}`)
+                copyText(sampleText)
+            })();
+        }
     }
 )
+
+const copyText = (text: string) => {
+    const inputElement = document.createElement("input");
+    inputElement.value = text;
+    document.body.appendChild(inputElement);
+    inputElement.focus();
+    inputElement.select();
+    // document.getSelection()?.selectAllChildren(inputElement);
+    const r = document.execCommand("copy");
+    document.body.removeChild(inputElement);
+    console.log(`return value: ${r}`)
+}
