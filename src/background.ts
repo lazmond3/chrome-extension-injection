c1cff024665f8baa1a9bc5a8159f1d749022e0d5
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-11-27 22:39:09 +0900

    add command and message: not worked

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,33 @@
 import { browser as chrome } from 'webextension-polyfill-ts'
 
+
+
+// () => {
+  // chrome.tabs.executeScript({
+  //   code: 'document.body.style.backgroundColor="orange"'
+  // });
+  
+// }
+
+(chrome.commands.onCommand as any).addListener(function(command:string, tab:{id: number}) {
+  console.log('Command:', command);
+
+  console.log(`typeof cmd: ${typeof(command)}, tab: ${tab} ${tab.id}, ${typeof(tab)},${typeof(tab.id)}`)
+  const tabId: number = tab.id
+
+  // コマンドはそのまま content scriptsに送信する。
+  // chrome.tabs.sendMessage(tabId, command)
+  chrome.tabs.executeScript({
+    file: 'inject.js'
+  });
+});
+
 // 結局polyfill に失敗しているじゃないか……
-chrome.runtime.onMessage.addListener(
-  function(message, callback) {
-    if (message == "runContentScript"){
-      chrome.tabs.executeScript({
-        file: 'inject.js'
-      });
-    }
- });
+// chrome.runtime.onMessage.addListener(
+//   function(message, callback) {
+//     if (message == "runContentScript"){
+//       chrome.tabs.executeScript({
+//         file: 'inject.js'
+//       });
+//     }
+//  });
