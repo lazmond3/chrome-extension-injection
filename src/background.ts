8767a263f09423127c25a4f9dbacdc66d1e69cf2
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-11-27 22:53:02 +0900

    comment out and onrequest

--- src/background.ts
+++ src/background.ts
@@ -1,32 +1,39 @@
-import { browser as chrome } from 'webextension-polyfill-ts'
-
-
+import { browser as chrome } from "webextension-polyfill-ts";
 
 // () => {
-  // chrome.tabs.executeScript({
-  //   code: 'document.body.style.backgroundColor="orange"'
-  // });
-  
+// chrome.tabs.executeScript({
+//   code: 'document.body.style.backgroundColor="orange"'
+// });
+
 // }
 
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) { return {cancel: true}; },
-  {urls: ["*://lazmond3.github.io/clear/*"]},
-  ["blocking"]);
+  function (details) {
+    return { cancel: true };
+  },
+  { urls: ["*://lazmond3.github.io/clear/*"] },
+  ["blocking"]
+);
 
+// (chrome.commands.onCommand as any).addListener(function (
+//   command: string,
+//   tab: { id: number }
+// ) {
+//   console.log("Command:", command);
 
-(chrome.commands.onCommand as any).addListener(function(command:string, tab:{id: number}) {
-  console.log('Command:', command);
+//   console.log(
+//     `typeof cmd: ${typeof command}, tab: ${tab} ${
+//       tab.id
+//     }, ${typeof tab},${typeof tab.id}`
+//   );
+//   const tabId: number = tab.id;
 
-  console.log(`typeof cmd: ${typeof(command)}, tab: ${tab} ${tab.id}, ${typeof(tab)},${typeof(tab.id)}`)
-  const tabId: number = tab.id
-
-  // コマンドはそのまま content scriptsに送信する。
-  // chrome.tabs.sendMessage(tabId, command)
-  chrome.tabs.executeScript({
-    file: 'inject.js'
-  });
-});
+//   // コマンドはそのまま content scriptsに送信する。
+//   // chrome.tabs.sendMessage(tabId, command)
+//   chrome.tabs.executeScript({
+//     file: "inject.js",
+//   });
+// });
 
 // 結局polyfill に失敗しているじゃないか……
 // chrome.runtime.onMessage.addListener(
