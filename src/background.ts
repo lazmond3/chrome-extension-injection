0310698c15477643586c777c8200c3a14b310275
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-11-27 22:55:30 +0900

    add constraint only js

--- src/background.ts
+++ src/background.ts
@@ -11,7 +11,7 @@ chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     return { cancel: true };
   },
-  { urls: ["*://lazmond3.github.io/clear/*"] },
+  { urls: ["*://lazmond3.github.io/clear/*.js"] },
   ["blocking"]
 );
 
