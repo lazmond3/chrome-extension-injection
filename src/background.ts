6abd31a35e3a3ad750ed80864b59eba4099ca23a
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-11-27 22:26:28 +0900

    update

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,11 @@
-import { browser } from 'webextension-polyfill-ts'
+import { browser as chrome } from 'webextension-polyfill-ts'
 
 // 結局polyfill に失敗しているじゃないか……
-(browser.commands.onCommand as any).addListener(function(command:string, tab:{id: number}) {
-  console.log('Command:', command);
-
-  console.log(`typeof cmd: ${typeof(command)}, tab: ${tab} ${tab.id}, ${typeof(tab)},${typeof(tab.id)}`)
-  const tabId: number = tab.id
-
-  // コマンドはそのまま content scriptsに送信する。
-  browser.tabs.sendMessage(tabId, command)
-});
+chrome.runtime.onMessage.addListener(
+  function(message, callback) {
+    if (message == "runContentScript"){
+      chrome.tabs.executeScript({
+        file: 'contentScript.js'
+      });
+    }
+ });
